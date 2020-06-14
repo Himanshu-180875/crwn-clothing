ab5c5cf5f7ae60269264350036e8b2eb7e12034f
Himanshu-180875 <himanshupahuja55@gmail.com>
2020-06-15 03:06:19 +0530

    created a sign in page

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,28 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+
+const config ={
+    apiKey: "AIzaSyAflWJc_Yt5JVRGSofFn729dYas0y5euv4",
+    authDomain: "crwn-db-186ee.firebaseapp.com",
+    databaseURL: "https://crwn-db-186ee.firebaseio.com",
+    projectId: "crwn-db-186ee",
+    storageBucket: "crwn-db-186ee.appspot.com",
+    messagingSenderId: "930050309072",
+    appId: "1:930050309072:web:12482532bd72643dc017d5",
+    measurementId: "G-D75NZ2P5SX"
+  };
+
+  firebase.initializeApp(config);
+
+  export const auth=firebase.auth();
+
+   export const firestore=firebase.firestore();
+
+   const provider =new firebase.auth.GoogleAuthProvider();
+   provider.setCustomParameters({promt:'select_account'});
+
+   export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+   export default firebase;
