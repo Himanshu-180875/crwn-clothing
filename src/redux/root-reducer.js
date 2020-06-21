8ff6fd4ba8c88da8841728082ace22967032d81a
Himanshu-180875 <himanshupahuja55@gmail.com>
2020-06-21 21:11:17 +0530

    modified root reducer and resolved issue of collection params

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,7 +1,7 @@
 import { combineReducers } from 'redux';
-import { persistReducer } from 'redux-persist'
-import {persistStore} from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
+import { persistReducer } from 'redux-persist';
+// import {persistStore} from 'redux-persist';
+import storage from   'redux-persist/lib/storage';
 
 import userReducer from './user/user-reducer';
 import cartReducer from './cart/cart.reducer';
@@ -9,9 +9,9 @@ import directoryReducer from './directory/directory.reducer';
 import shopReducer from './shop/shop.reducer';
 
 const persistConfig = {
-  key:'root',
+  key: 'root',
   storage,
-  whiteList: ['cart']
+  whitelist: ['cart']
 }
 
 const rootReducer = combineReducers({
